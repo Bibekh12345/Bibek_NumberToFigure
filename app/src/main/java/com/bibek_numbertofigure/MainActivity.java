1cc0aa3ff0443e53196bdeae740c78e1603e31da
Softwarica College Lab <softwarica@softwarica.com>
2019-04-12 17:13:34 +0545

    changes

--- app/src/main/java/com/bibek_numbertofigure/MainActivity.java
+++ app/src/main/java/com/bibek_numbertofigure/MainActivity.java
@@ -2,12 +2,40 @@ package com.bibek_numbertofigure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNumber;
+    TextView tvWords;
+    Button btnConvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNumber= findViewById(R.id.etNumber);
+        tvWords= findViewById(R.id.tvWords);
+        btnConvert= findViewById(R.id.btnConvert);
+
+
+        btnConvert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+
+                int num;
+                num= Integer.parseInt(etNumber.getText().toString());
+
+                Bibek_NumToWord numbertoWords= new Bibek_NumToWord(num);
+                tvWords.setText(numbertoWords.Words());
+
+            }
+        });
+
     }
 }
